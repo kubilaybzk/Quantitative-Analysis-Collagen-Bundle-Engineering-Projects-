1317286ffd1e643375b0428d21252203155683dc
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-05-18 00:37:42 +0300

    s
    s

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def mypolygon_perimeter(X, Y, image_shape):
 
 
 # read images
-imgs = cv2.imreadmulti("health1.tif", flags=cv2.IMREAD_GRAYSCALE + cv2.IMREAD_ANYDEPTH)[1]
+imgs = cv2.imreadmulti("health.tif", flags=cv2.IMREAD_GRAYSCALE + cv2.IMREAD_ANYDEPTH)[1]
 
 for i, img in enumerate(imgs):
     filename = f"face_1_frame-{i}.png"
@@ -384,10 +384,57 @@ for i in range(0,512):
     for k in range(0,512):
         if(img[i][k]>0):
             img[i][k] = 1
-cv2.imshow("img2",img)
+cv2.imshow("BlackandWhite",img)
+plt.imsave("Dilated Image With Defauld_BlackandWhite.png",img)
 cv2.waitKey(0)
 img2 = img_as_float(io.imread('face_1_frame-0.png', as_gray=True))
 result=img2*img
 plt.imsave("Dilated Image With Defauld.png",result)
 cv2.imshow("result",result)
 cv2.waitKey(0)
+
+
+
+
+
+
+img = cv2.imread('Dilated Image With Defauld.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+kernel_size = 5
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+
+low_threshold = 50
+high_threshold = 150
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+rho = 1  # distance resolution in pixels of the Hough grid
+theta = np.pi / 180  # angular resolution in radians of the Hough grid
+threshold = 60 #15  # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 70 #50  # minimum number of pixels making up a line
+max_line_gap = 20  # maximum gap in pixels between connectable line segments
+line_image = np.copy(img) * 0  # creating a blank to draw lines on
+
+# Run Hough on edge detected image
+# Output "lines" is an array containing endpoints of detected line segments
+lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                    min_line_length, max_line_gap)
+
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+
+# Draw the lines on the  image
+lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+
+cv2.imshow("lines_edges",lines_edges)
+plt.imsave("lines_edges.png",lines_edges)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
